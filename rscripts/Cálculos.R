49ebc25b524e8bdf2bf3a033eb75bf68fb9924cf
malessandrello <mauricio.alessandrello@gmail.com>
2023-12-30 19:28:18 -0300

    Cálculos

--- rscripts/Cálculos.R
+++ rscripts/Cálculos.R
@@ -1,14 +1,14 @@
 library(dplyr)
 library(tidyverse)
 
-AN <- deg_htp2 %>% filter(Tratamiento == "AN") %>% select(HTP)
-BS <- deg_htp2 %>% filter(Tratamiento == "bs") %>% select(HTP)
-BioEst <- deg_htp2 %>% filter(Tratamiento == "bioest") %>% select(HTP)
-P26 <- deg_htp2 %>% filter(Tratamiento == "P26") %>% select(HTP)
 
+#Se obtienen los valores iniciales de la concentración de HTP y se calcula su promedio
 inicial <- deg_htp2 %>% filter(Tiempo == 0) %>% pull(HTP)
 
 t0 <- mean(inicial)
+
+#Se obtienen valores finales de la concentración de HTP para cada tratamiento
+
 
 finalAN <- deg_htp2 %>% filter(Tiempo == 139 & Tratamiento == "AN") %>% pull(HTP)
 
@@ -19,21 +19,35 @@ finalBioEst <- deg_htp2 %>% filter(Tiempo == 139 & Tratamiento == "bioest") %>% 
 finalP26 <- deg_htp2 %>% filter(Tiempo == 139 & Tratamiento == "P26") %>% pull(HTP)
 
 
+#Se crean variables para el cálculo de intervalos de confianza 95%
 
-dAN<-t0 - finalAN
-dBS<-t0 - finalBS
-dBioEst<-t0 - finalBioEst
-dP26<-t0 - finalP26
+finalBIO <- c(finalBS, finalP26, finalBioEst) # Concentraciones finales de tratamientos biológicos, se consideran como réplicas los distintos tratamientos porque no evidenciaron diferencias.
+avgBIO<- mean(finalBIO) # su promedio
+N <- length(finalBIO) # número de réplicas
+desvest <- sd(finalBIO) # su desviación estándar
+moe <- qt(0.975, 2) * desvest/sqrt(N) #margen de error usando una distribución t.
+moe_perc <- moe/avgBIO*100 #margen de error en %
+finalAN1 <- c(finalAN, finalAN + moe_perc*finalAN/100, finalAN - moe_perc*finalAN/100)# concentraciones finales del tratamiento AN. Se considera una variación de más/menos moe_perc obtenida para los tratamientos BIO
+avgAN <- mean(finalAN1) 
 
-finalt <- c(finalBS, finalBioEst, finalP26)
-finald<- c(dBS, dBioEst, dP26)
-avg<- mean(finald)
-length(finald)
-sd(finald)
-moe <- qt(0.975, 2) * sd(finald)/sqrt(length(finald))
-avg + moe
-avg - moe
-t0 - (avg - moe)
+lim_supBIo <- avgBIO + moe
+lim_infBIO <- avgBIO - moe
+
+lim_supAN <- avgAN + moe
+lim_infAN <- avgAN - moe
+
+t.test(finalAN1, finalBIO)
+
+lim_supBIo
+lim_infBIO
+
+lim_supAN
+lim_infAN
+
+
+
+
+
 
 
 
